33f1f1cabe496127a8b5d64e0a1d53034c94a535
muyu-tarai <hello.tra.murai@gmail.com>
2023-09-15 13:33:03 +0900

    menusテーブル作成完了

--- database/migrations/2023_09_15_041056_create_menus_table.php
+++ database/migrations/2023_09_15_041056_create_menus_table.php
@@ -14,7 +14,23 @@ class CreateMenusTable extends Migration
     public function up()
     {
         Schema::create('menus', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->integer('store_id')
+                ->unsigned();
+            $table->foreign('store_id')
+                ->references('id')
+                ->on('stores')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+            $table->string('menu_name', 20);
+            $table->string('menu_image', 255)
+                ->nullable(true);
+            $table->integer('price');
+            $table->tinyInteger('sold_out_flag')
+                ->default(0)
+                ->comment('0:販売中,1:売り切れ');
+            $table->text('menu_comment')
+                ->nullable(true);
             $table->timestamps();
         });
     }
