e2247f406033b68751f327489cae3191e6729350
muyu-tarai <hello.tra.murai@gmail.com>
2023-09-18 12:20:18 +0900

    margeする前に一時保存

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,10 @@
 @endsection
 <link rel="stylesheet" href="/css/murai.css">
 @section('content')
-<div>
-
+<div class="top-section">
+  <div>
+    <h1>出店様へ</h1>
+    <p>このサイトはフードカーを盛り上げるために制作されたサイトです。もし興味がありましたら、下のアイコンから登録をお願いします。</p>
+  </div>
 </div>
 @endsection
