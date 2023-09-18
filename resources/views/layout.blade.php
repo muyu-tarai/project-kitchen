8da71191836ebc7a1977643fc22a6085f4127ef4
muyu-tarai <hello.tra.murai@gmail.com>
2023-09-18 10:06:15 +0900

    ヘッダーフッターの共通レイアウト完成

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,8 +1,7 @@
-@extends('preloginLayout')
+@extends('preLoginLayout')
 @section('header_right')
 <div class="header-right medium">
-  <a href="#">出店</a>
-  <a href="#">登録</a>
+  <a href="#">出店準備</a>
   <a href="#">マイページ</a>
   <a href="#">ログアウト</a>
 </div>
