955eb1c11240924eec15e64715b83a7a036c5506
muyu-tarai <hello.tra.murai@gmail.com>
2023-09-20 12:25:26 +0900

    gitignoreテストのため一時コミット

--- app/Http/Controllers/storeRegisterController.php
+++ app/Http/Controllers/storeRegisterController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Store;
+use App\Menu;
 
 use Illuminate\Http\Request;
 
@@ -13,7 +15,23 @@ class storeRegisterController extends Controller
 
     public function tmp(Request $request)
     {
-        dd($request);
+
+        // フォルダモデルのインスタンスを作成する
+        $store = new Store();
+        $menu = new Menu();
+        // タイトルに入力値を代入する
+        $store->user_id = 1;
+        $store->store_name = $request->store_name;
+        $store->store_image = $request->store_image;
+        $store->store_comment = $request->store_comment;
+        $menu->menu_name = $request->menu_name;
+        $menu->menu_image = $request->menu_image;
+        $menu->price = $request->menu_price;
+        $menu->menu_comment = $request->menu_comment;
+        // インスタンスの状態をデータベースに書き込む
+        $store->save();
+        $menu->save();
+
         return view('tmp');
     }
 }
--- resources/views/storeRegister.blade.php
+++ resources/views/storeRegister.blade.php
@@ -9,7 +9,7 @@
   @csrf
   <div class="inner-top">
     <h2>店舗名</h2>
-    <input type="text" name="store-name">
+    <input type="text" name="store_name">
     <h2>店舗画像</h2>
     <img src="/images/icons/noImage.jpg" class="store-img" alt="">
     <div class="for-button">
@@ -17,20 +17,20 @@
         <button></button>画像を削除
       </label>
       <label>
-        <input type="file" name="store-img">写真を選択
+        <input type="file" name="store_image">写真を選択
       </label>
     </div>
   </div>
   <div class="inner-middle">
     <div>
       <h2>店舗紹介コメント</h2>
-      <textarea name="store-comment" id="" cols="30" rows="10"></textarea>
+      <textarea name="store_comment" id="" cols="30" rows="10"></textarea>
     </div>
     <div class="stores-items">
       <div class="stores-item">
         <img src="/images/foods/Crepe1.jpg" alt="クレープ">
         <p class="menu-title">チョコバナナ生クレープ</p>
-        <p class="menu-amount">4600円</p>
+        <p class="menu-price">4600円</p>
         <p class="menu-text">
           クレープの王道チョコバナナ！
           チョコの甘さと特製生クリームの
@@ -42,7 +42,7 @@
       <div class="stores-item">
         <img src="/images/foods/Crepe4.jpg" alt="クレープ">
         <p class="menu-title">クラシックストロベリー</p>
-        <p class="menu-amount">400円</p>
+        <p class="menu-price">400円</p>
         <p class="menu-text">
           新鮮な苺とホイップクリームの贅沢な組み合わせ。
         </p>
@@ -51,7 +51,7 @@
       <div class="stores-item">
         <img src="/images/foods/Crepe3.jpg" alt="クレープ">
         <p class="menu-title">サボテンのオアシス</p>
-        <p class="menu-amount">660円</p>
+        <p class="menu-price">660円</p>
         <p class="menu-text">
           クリームチーズとキウイの爽やかなハーモニー。
         </p>
@@ -60,7 +60,7 @@
       <div class="stores-item">
         <img src="/images/foods/Crepe4.jpg" alt="クレープ">
         <p class="menu-title">シナモンアップルクラシック</p>
-        <p class="menu-amount">500円</p>
+        <p class="menu-price">500円</p>
         <p class="menu-text">
           シナモン調のりんごとシロップの美味しさ。
         </p>
@@ -80,24 +80,24 @@
             <button></button>画像を削除
           </label>
           <label>
-            <input type="file" name="menu-img">写真を選択
+            <input type="file" name="menu_image">写真を選択
           </label>
         </div>
       </div>
       <div>
         <h3>メニュー名</h3>
-        <input type="text" name="menu-name">
+        <input type="text" name="menu_name">
       </div>
       <div>
         <h3>メニュー金額</h3>
-        <div class="add-menu-amount">
-          <input type="text" name="menu-amount">
+        <div class="add-menu-price">
+          <input type="text" name="menu_price">
           <p>円</p>
         </div>
       </div>
       <div>
         <h3>メニュー紹介コメント</h3>
-        <textarea name="menu-comment" id="" cols="30" rows="10"></textarea>
+        <textarea name="menu_comment" id="" cols="30" rows="10"></textarea>
       </div>
       <div class="for-button">
         <label>
