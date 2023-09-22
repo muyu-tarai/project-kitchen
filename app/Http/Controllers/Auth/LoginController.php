e0af29cef001241edd9bd4245741d0748dad04cf
satou4649 <hello.tra.satou.t@gmail.com>
2023-09-22 11:38:23 +0900

    ユーザー登録

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/mypage';
+    protected $redirectTo = '/store_register';
 
     /**
      * Create a new controller instance.
