b77de2daada5d3eb480bc9f7679624261ee79f4a
ookawayuuki <hello.tra.ookawa@gmail.com>
2023-10-02 15:44:05 +0900

    一旦マスターに上げます

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+Route::get('/','IndexController@index');
+
 Route::get('/store/{id}','ViewController@view');
-Route::post('/store_update', 'updateController@update');
 
 Route::get('/store_update', 'updateController@store_update')->name('user_update');
 Route::post('/store_update', 'updateController@update');
