3f9d6161947f12eed304e875b35edac1f5f52cec
muyu-tarai <hello.tra.murai@gmail.com>
2023-10-03 09:57:25 +0900

    qevert "機能実装完了"

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -20,6 +20,7 @@ class IndexController extends Controller
             $storeImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
             $closeStores[$key]->store_image = $storeImageFromDropbox;
         }
+        dd($closeStores);
         return view('index', [
             'close_stores' => $closeStores,
             'open_stores' => $openStores,
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,28 +20,29 @@
       <h1 class="large">オープン中の店舗</h1>
     </div>
     <div class="stores-items">
-      @foreach($open_stores as $open_stores )
-      <div class="stores-item">
-        <a href="store/{{ $open_stores->id}}"><img src="data:image/{{ $open_stores->ext }};base64,{{ $open_stores->store_image }}"></a>
-        <p class="medium">>{{ isset($open_stores->store_name) ? $open_stores->store_name : '' }}</p>
-      </div>
+      @foreach($open_stores  as $open_stores )
+        <div class="stores-item">
+          <a href="store/{{ $open_stores->id}}"><img src="{{ $open_stores->store_image }}"></a>
+          <p class="medium">>{{ isset($open_stores->store_name) ? $open_stores->store_name : '' }}</p>
+        </div>
       @endforeach
     </div>
+
     <div class="stores-close">
       <div class="stores-title">
         <h1 class="large">クローズ中の店舗</h1>
       </div>
-      <div class="stores-items">
-        @foreach($close_stores as $close_stores )
+      <div class="storess-items">
+      @foreach($close_stores  as $close_stores )
         <div class="stores-item">
-          <a href="store/{{ $close_stores->id}}"><img src="data:image/{{ $close_stores->ext }};base64,{{ $close_stores->store_image }}"></a>
+          <a href="store/{{ $close_stores->id}}"><img src="{{ $close_stores->store_image }}"></a>
           <p class="medium">{{ isset($close_stores->store_name) ? $close_stores->store_name : '' }}</p>
         </div>
         @endforeach
       </div>
     </div>
   </div>
-  @endsection
-  @section('js')
-  <script src="/js/index.js "></script>
-  @endsection
+    @endsection
+    @section('js')
+    <script src="/js/index.js "></script>
+    @endsection
