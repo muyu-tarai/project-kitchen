acd25aa8a4b4f5346f8a58404755bb4417e77a51
satou4649 <hello.tra.satou.t@gmail.com>
2023-10-13 15:12:29 +0900

    修正完了

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -8,42 +8,13 @@
 
 <div class="imag-car">
   <style>
-    .imag-car{
-    background-image: url("data: image/jpg;base64,{{ base64_encode(Storage::disk('dropbox')->get('view/truck9.jpg')) }}");
+    .imag-car {
+      background-image: url("data: image/jpg;base64,{{ base64_encode(Storage::disk('dropbox')->get('view/truck9.jpg')) }}");
     }
   </style>
   <div class="container">
     <div class="row">
 
-<<<<<<< HEAD
-        <p class="panel-heading">マイページ</p>
-        <div>
-          <form action="{{ route('mypage') }}" method="post">
-            @csrf
-            <div class="panel-body">
-              <div class="about">
-                <label for="name">ユーザー名</label>
-                @error('name')
-                <span class="alert" role="alert">{{ $message }}</span>
-                @enderror
-              </div>
-              <div class="input">
-                <input type="text" class="form-control" id="name" name="name" value="{{ Auth::user()->name }}">
-              </div>
-              <div class="email">
-                <p>メールアドレス</p>
-              </div>
-              <div class="email2">
-                <p>{{ Auth::user()->email }}</p>
-              </div>
-
-              <div class="button">
-                @if(Auth::user()->email !== 'gest@gmail.com')
-                <input type="submit" class="updeta" formaction="{{ route('mypage') }}" value="更新">
-                <input type="submit" class="Withdrawal" formaction="{{ route('leave_account') }}" value="退会">
-                @endif
-              </div>
-=======
       <p class="panel-heading">マイページ</p>
       <div>
         <form action="{{ route('mypage') }}" method="post">
@@ -54,7 +25,6 @@
               @error('name')
               <span class="alert" role="alert">{{ $message }}</span>
               @enderror
->>>>>>> master
             </div>
             <div class="input">
               <input type="text" class="form-control" id="name" name="name" value="{{ Auth::user()->name }}">
@@ -67,12 +37,13 @@
             </div>
 
             <div class="button">
-              @if(Auth::user()->id !== 42)
+              @if(Auth::user()->email !== 'gest@gmail.com')
               <input type="submit" class="updeta" formaction="{{ route('mypage') }}" value="更新">
               <input type="submit" class="Withdrawal" formaction="{{ route('leave_account') }}" value="退会">
               @endif
             </div>
           </div>
+
         </form>
         <form action="{{ route('leave_account') }}"></form>
       </div>
