05c375cc07936df1742486212deab6fc6ae98684
ookawayuuki <hello.tra.ookawa@gmail.com>
2023-10-17 11:08:47 +0900

    dd1

--- resources/views/preloginLayout.blade.php
+++ resources/views/preloginLayout.blade.php
@@ -11,11 +11,10 @@
   @yield('css')
   <script src="https://kit.fontawesome.com/66cf41360e.js" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-  <meta na(me="csrf-token" content="{{ csrf_token() }}">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
-  {{dd('throuw')}}
   <div class="header">
     <header>
       <nav class="navbar">
