ee5b8509e30b93ff1b693de3cde5886215217dce
muyu-tarai <hello.tra.murai@gmail.com>
2023-10-18 11:57:18 +0900

    ログアウトがセキュアじゃないっぽいから消してみる

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,10 +10,10 @@
   <a href="/store_update">出店準備</a>
   <a href="/store_register">登録内容変更</a>
   <a href="/mypage">マイページ</a>
-  <a href="/logout">ログアウト</a>
+  <!-- <a href="/logout">ログアウト</a> -->
 </div>
 @endif
-<form action="{{ route('logout') }}" method="post" id="logout-form">@csrf</form>
+<!-- <form action="{{ route('logout') }}" method="post" id="logout-form">@csrf</form> -->
 @endsection
 
 @section('scripts')
