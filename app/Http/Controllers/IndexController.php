2ca395f111685fe1d01b5cc2ae6eb0ff6273ebf3
muyu-tarai <hello.tra.murai@gmail.com>
2023-10-18 14:22:59 +0900

    indexの下半分消してlolした

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Store;
-use Illuminate\Support\Facades\Storage;
+// use Illuminate\Support\Facades\Storage;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\File;
+// use Illuminate\Support\Facades\File;
 // use Illuminate\Http\Request;
 // use App\index;
 // use Illuminate\Support\Facades\DB;
@@ -14,6 +14,7 @@ class IndexController extends Controller
 {
     public function index()
     {
+        dd('lol');
         $store_items = Store::where('opening_flag', 1)->get();
 
         $i = 0;
@@ -30,25 +31,25 @@ class IndexController extends Controller
         $closeStores = Store::where('opening_flag', 0)->get();
         $openStores = Store::where('opening_flag', 1)->get();
 
-        foreach ($openStores as $key => $store) {
-            if (Storage::disk('dropbox')->exists($store->store_image)) {
-                $openStores[$key]->ext = File::extension($store->store_image);
-                $openStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-                $openStores[$key]->store_image = $openStoreImageFromDropbox;
-            }
-        }
+        // foreach ($openStores as $key => $store) {
+        //     if (Storage::disk('dropbox')->exists($store->store_image)) {
+        //         $openStores[$key]->ext = File::extension($store->store_image);
+        //         $openStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
+        //         $openStores[$key]->store_image = $openStoreImageFromDropbox;
+        //     }
+        // }
 
-        foreach ($closeStores as $key => $store) {
-            if (Storage::disk('dropbox')->exists($store->store_image)) {
-                $closeStores[$key]->ext = File::extension($store->store_image);
-                $closeStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-                $closeStores[$key]->store_image = $closeStoreImageFromDropbox;
-            }
-        }
+        // foreach ($closeStores as $key => $store) {
+        //     if (Storage::disk('dropbox')->exists($store->store_image)) {
+        //         $closeStores[$key]->ext = File::extension($store->store_image);
+        //         $closeStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
+        //         $closeStores[$key]->store_image = $closeStoreImageFromDropbox;
+        //     }
+        // }
 
-        return view('index', [
-            'close_stores' => $closeStores,
-            'open_stores' => $openStores,
-        ]);
+        // return view('index', [
+        //     'close_stores' => $closeStores,
+        //     'open_stores' => $openStores,
+        // ]);
     }
 }
