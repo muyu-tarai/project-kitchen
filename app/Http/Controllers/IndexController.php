9e57fee75cfe775fcc7f3932223fe95e5aab0be2
muyu-tarai <hello.tra.murai@gmail.com>
2023-10-18 11:15:36 +0900

    indexの->store_imageにemptyだったら被せた

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,46 +2,45 @@
 
 namespace App\Http\Controllers;
 
-// use App\Store;
-// use Illuminate\Support\Facades\Storage;
-// use Carbon\Carbon;
+use App\Store;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
 // use Illuminate\Http\Request;
 // use App\index;
-// use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\File;
 // use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        dd('lol');
-    //     $store_items = Store::where('opening_flag',1)->get();
+        $store_items = Store::where('opening_flag',1)->get();
         
-    //     $i = 0;
-    //     foreach ($store_items as $store_item) {
-    //         if( $store_item->closing_datetime < Carbon::now() ){
-    //             $store = Store::firstwhere('id',$store_item->id);
-    //             $store->opening_flag = 0;
-    //             $store->save();
-    //         }
-    //         $i++;
-    //     }
+        $i = 0;
+        foreach ($store_items as $store_item) {
+            if( $store_item->closing_datetime < Carbon::now() ){
+                $store = Store::firstwhere('id',$store_item->id);
+                $store->opening_flag = 0;
+                $store->save();
+            }
+            $i++;
+        }
         
-    //     $closeStores = Store::where('opening_flag', 0)->get();
-    //     $openStores = Store::where('opening_flag', 1)->get();
-    //     foreach ($openStores as $key => $store) {
-    //         $openStores[$key]->ext = File::extension($store->store_image);
-    //         $openStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-    //         $openStores[$key]->store_image = $openStoreImageFromDropbox;
-    //     }
-    //     foreach ($closeStores as $key => $store) {
-    //         $closeStores[$key]->ext = File::extension($store->store_image);
-    //         $closeStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-    //         $closeStores[$key]->store_image = $closeStoreImageFromDropbox;
-    //     }
-    //     return view('index', [
-    //         'close_stores' => $closeStores,
-    //         'open_stores' => $openStores,
-    //     ]);
+        $closeStores = Store::where('opening_flag', 0)->get();
+        $openStores = Store::where('opening_flag', 1)->get();
+        foreach ($openStores as $key => $store) {
+            $openStores[$key]->ext = File::extension(!empty($store->store_image));
+            $openStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get(!empty($store->store_image)));
+            $openStores[$key]->store_image = $openStoreImageFromDropbox;
+        }
+        foreach ($closeStores as $key => $store) {
+            $closeStores[$key]->ext = File::extension(!empty($store->store_image));
+            $closeStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get(!empty($store->store_image)));
+            $closeStores[$key]->store_image = $closeStoreImageFromDropbox;
+        }
+        return view('index', [
+            'close_stores' => $closeStores,
+            'open_stores' => $openStores,
+        ]);
     }
 }
