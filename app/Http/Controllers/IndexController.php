62d0470fe6f7d6f0cc972e02807a379493f8b854
muyu-tarai <hello.tra.murai@gmail.com>
2023-10-18 15:07:25 +0900

    index下半分削除しreturnでviewだけ返してみる

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -30,25 +30,7 @@ class IndexController extends Controller
         $closeStores = Store::where('opening_flag', 0)->get();
         $openStores = Store::where('opening_flag', 1)->get();
         
-        foreach ($openStores as $key => $store) {
-            if (is_string($store->store_image) && Storage::disk('dropbox')->exists($store->store_image)) {
-                $openStores[$key]->ext = File::extension($store->store_image);
-                $openStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-                $openStores[$key]->store_image = $openStoreImageFromDropbox;
-            }
-        }
 
-        foreach ($closeStores as $key => $store) {
-            if (is_string($store->store_image) && Storage::disk('dropbox')->exists($store->store_image)) {
-                $closeStores[$key]->ext = File::extension($store->store_image);
-                $closeStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-                $closeStores[$key]->store_image = $closeStoreImageFromDropbox;
-            }
-        }
-
-        return view('index', [
-            'close_stores' => $closeStores,
-            'open_stores' => $openStores,
-        ]);
+        return view('index');
     }
 }
