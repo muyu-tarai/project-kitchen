19d3577b0750ef0c5ce9b5edf6c7d69799ca6e14
muyu-tarai <hello.tra.murai@gmail.com>
2023-10-18 10:39:47 +0900

    トールさんのサインアップバリ+退会のモーダル途中

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,47 +16,37 @@
         <div class="col col-md-offset-3 col-md-6">
 
           <div class="panel-heading">SIGN UP</div>
+          @if($errors->any())
+          <div class="err">
+            @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+            @endforeach
+          </div>
+          @endif
           <div class="panel-body">
             <form action="{{ route('register') }}" method="POST">
               @csrf
               <div class="form-group">
                 <label for="name" class="alert">username</label><br>
-                @error('name')
-                <span role="alert">{{ $message }}</span>
-                @enderror
 
                 <div class="input">
                   <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" />
                 </div>
               </div>
-
               <div class="form-group">
-
                 <label for="email" class="alert">mail</label><br>
-                @error('email')
-                <span role="alert">{{ $message }}</span>
-                @enderror
-
                 <div class="input">
                   <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" />
                 </div>
               </div>
               <div class="form-group">
-
                 <label for="password" class="alert">password</label><br>
-                @error('password')
-                <span role="alert">{{ $message }}</span>
-                @enderror
-
                 <div class="input">
                   <input type="password" class="form-control" id="password" name="password">
                 </div>
-
               </div>
               <div class="form-group">
-
                 <label for="password-confirm" class="alert">password check</label><br>
-
                 <div class="input">
                   <input type="password" class="form-control" id="password-confirm" name="password_confirmation">
                 </div>
@@ -68,7 +58,6 @@
         </div>
         </form>
       </div>
-
     </div>
   </div>
   </div>
