494ccab94f4221206b39edc4fd36c291f18adbb6
muyu-tarai <hello.tra.murai@gmail.com>
2023-10-18 11:31:51 +0900

    indexの中がlol

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,55 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use App\Store;
-use Illuminate\Support\Facades\Storage;
-use Carbon\Carbon;
+// use App\Store;
+// use Illuminate\Support\Facades\Storage;
+// use Carbon\Carbon;
+// use Illuminate\Support\Facades\File;
 // use Illuminate\Http\Request;
 // use App\index;
-use Illuminate\Support\Facades\File;
 // use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        $store_items = Store::where('opening_flag', 1)->get();
+        dd('lol');
+        // $store_items = Store::where('opening_flag', 1)->get();
 
-        $i = 0;
+        // $i = 0;
 
-        if (!empty($store_items)) {
-            foreach ($store_items as $store_item) {
-                if ($store_item->closing_datetime < Carbon::now()) {
-                    $store = Store::firstwhere('id', $store_item->id);
-                    $store->opening_flag = 0;
-                    $store->save();
-                }
-                $i++;
-            }
-        }
+        // if (!empty($store_items)) {
+        //     foreach ($store_items as $store_item) {
+        //         if ($store_item->closing_datetime < Carbon::now()) {
+        //             $store = Store::firstwhere('id', $store_item->id);
+        //             $store->opening_flag = 0;
+        //             $store->save();
+        //         }
+        //         $i++;
+        //     }
+        // }
 
-        $closeStores = Store::where('opening_flag', 0)->get();
-        $openStores = Store::where('opening_flag', 1)->get();
+        // $closeStores = Store::where('opening_flag', 0)->get();
+        // $openStores = Store::where('opening_flag', 1)->get();
 
-        if (!empty($openStores)) {
-            foreach ($openStores as $key => $store) {
-                $openStores[$key]->ext = File::extension($store->store_image);
-                $openStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-                $openStores[$key]->store_image = $openStoreImageFromDropbox;
-            }
-        }
+        // if (!empty($openStores)) {
+        //     foreach ($openStores as $key => $store) {
+        //         $openStores[$key]->ext = File::extension($store->store_image);
+        //         $openStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
+        //         $openStores[$key]->store_image = $openStoreImageFromDropbox;
+        //     }
+        // }
 
-        if (!empty($closeStores)) {
-            foreach ($closeStores as $key => $store) {
-                $closeStores[$key]->ext = File::extension($store->store_image);
-                $closeStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-                $closeStores[$key]->store_image = $closeStoreImageFromDropbox;
-            }
-        }
+        // if (!empty($closeStores)) {
+        //     foreach ($closeStores as $key => $store) {
+        //         $closeStores[$key]->ext = File::extension($store->store_image);
+        //         $closeStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
+        //         $closeStores[$key]->store_image = $closeStoreImageFromDropbox;
+        //     }
+        // }
 
-        return view('index', [
-            'close_stores' => $closeStores,
-            'open_stores' => $openStores,
-        ]);
+        // return view('index', [
+        //     'close_stores' => $closeStores,
+        //     'open_stores' => $openStores,
+        // ]);
     }
 }
