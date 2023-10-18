68ce781e240a705f32794b7b8f18d8f607d14c4e
yuta <hello.tra.murai@gmail.com>
2023-10-18 23:16:06 +0900

    いかないのでmypageは戻した

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -2,53 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Store;
-use Illuminate\Support\Facades\Storage;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\File;
-// use Illuminate\Http\Request;
-// use App\index;
-// use Illuminate\Support\Facades\DB;
+use App\Http\Requests\edituser;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class IndexController extends Controller
+class MypageController extends Controller
 {
-    public function index()
+    public function mypage()
     {
-        $store_items = Store::where('opening_flag', 1)->get();
-        
-        $i = 0;
+        return view('mypage');
+    }
 
-        foreach ($store_items as $store_item) {
-            if ($store_item->closing_datetime < Carbon::now()) {
-                $store = Store::firstwhere('id', $store_item->id);
-                $store->opening_flag = 0;
-                $store->save();
-            }
-            $i++;
-        }
-        
-        $closeStores = Store::where('opening_flag', 0)->get();
-        $openStores = Store::where('opening_flag', 1)->get();
-        
-        foreach ($openStores as $key => $store) {
-            if (is_string($store->store_image) && Storage::disk('dropbox')->exists($store->store_image)) {
-                $openStores[$key]->ext = File::extension($store->store_image);
-                $openStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-                $openStores[$key]->store_image = $openStoreImageFromDropbox;
-            }
-        }
+    
+    // ユーザー名を変更する
+    public function edit(edituser $request)
+    {
+        $user = User::find(Auth::user()->id);
+        $user->name = $request->name;
+        $user->save();
 
-        foreach ($closeStores as $key => $store) {
-            if (is_string($store->store_image) && Storage::disk('dropbox')->exists($store->store_image)) {
-                $closeStores[$key]->ext = File::extension($store->store_image);
-                $closeStoreImageFromDropbox = base64_encode(Storage::disk('dropbox')->get($store->store_image));
-                $closeStores[$key]->store_image = $closeStoreImageFromDropbox;
-            }
-        }
-
-        return view('index', [
-            'close_stores' => $closeStores,
-            'open_stores' => $openStores,
-        ]);
+        // 3
+        return redirect()->route('mypage');
     }
 }
