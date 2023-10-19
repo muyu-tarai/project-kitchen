b08948e53d54c2c8ec1ff51a59f288ecf2f5d0cd
satou4649 <hello.tra.satou.t@gmail.com>
2023-10-19 09:57:55 +0900

    モーダルデザイン修正

--- public/js/leave_account.js
+++ public/js/leave_account.js
@@ -1,11 +1,13 @@
 document.getElementById("modal").addEventListener("click", function() { 
   document.getElementById('myModal').style.display = 'block';
   document.getElementById('button').style.display = 'none';
+  
 });
 
 document.getElementById("close").addEventListener("click", function() { 
   document.getElementById('myModal').style.display = 'none';
   document.getElementById('button').style.display = 'flex';
   document.getElementById("error").textContent ='';
+  
 });
 
