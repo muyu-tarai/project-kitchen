76de86631c5fb856498b17869644ad895d7a87e1
yuta <hello.tra.murai@gmail.com>
2023-11-16 00:43:19 +0900

    cache.phpのhostを明示的にした

--- config/cache.php
+++ config/cache.php
@@ -64,7 +64,7 @@ return [
             ],
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'host' => env('kitchencar-station.onrender.com', '127.0.0.1'),
                     'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
